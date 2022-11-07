ae467276e981203ee1d670150c5b7fff30d9127b
Suwook Ham <suwookh@gmail.com>
2022-11-07 00:25:07 -0800

    Update for directory parameter

--- argtest.py
+++ argtest.py
@@ -3,9 +3,12 @@ import argparse
 def arginit():
   parser = argparse.ArgumentParser()
   parser.add_argument("-d", "--dir", help="Director Name", required=True)
-  return parser
+  args = parser.parse_args()
+  return args
 
 if __name__ == "__main__":
   # if used by script call, not as module
   print("Arg Parser test code")
   args = arginit()
+  if args.dir:
+    print(args.dir)
