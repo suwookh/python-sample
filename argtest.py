59d029c1323f8cf314daa421be68820867b0767a
Suwook Ham <suwookh@gmail.com>
2023-09-03 23:20:39 -0700

    Updated

--- argtest.py
+++ argtest.py
@@ -1,14 +1,21 @@
 import argparse
+import time
+
+def secwait(sec):
+    sec = int(sec)
+    for i in range(0, sec):
+      time.sleep(1)
+      print(i+1, "seconds.")
 
 def arginit():
-  parser = argparse.ArgumentParser()
-  parser.add_argument("-d", "--dir", help="Director Name", required=True)
-  args = parser.parse_args()
-  return args
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-s", "--second", help="Seconds to print.")
+    args = parser.parse_args()
+    return args
 
 if __name__ == "__main__":
-  # if used by script call, not as module
-  print("Arg Parser test code")
-  args = arginit()
-  if args.dir:
-    print(args.dir)
+    # if used by script call, not as module
+    print("Arg Parser test code")
+    args = arginit()
+    if args.second:
+        secwait(args.second)
